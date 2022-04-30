f85760a7b63a1e6d15167148da09cd9569f93ca6
Collins <mutaic84@gmail.com>
2022-04-30 19:19:34 +0300

    enclose content in a div

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,11 @@ console.log(blogData);
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Header name={blogData.name}/>
       <About image={blogData.image} about={blogData.about} />
       <ArticleList posts={blogData.posts}/>
-    </>
+    </div>
   );
 }
 
