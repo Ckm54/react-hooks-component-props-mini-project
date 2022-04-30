b5f339ddcae079d1773d3936de6d9b97e838c044
Collins <mutaic84@gmail.com>
2022-04-30 17:33:23 +0300

    display each article data - test

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,7 +1,13 @@
 import React from "react"
 
-function Article() {
-  return <h2>Article oj</h2>
+function Article({title, date="January 1, 1970", preview}) {
+  return(
+    <>
+      <h3>{title}</h3>
+      <small>{date}</small>
+      <p>{preview}</p>
+    </>
+  )
 }
 
 export default Article
