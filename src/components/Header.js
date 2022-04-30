d3ced9b83fd6bdc0d80ee9de195cc6620b6f72d7
Collins <mutaic84@gmail.com>
2022-04-30 19:20:07 +0300

    enclose heading in header tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from "react"
 
 function Header(props) {
-  return <h1>{props.name}</h1>
+  return (
+    <header>
+      <h1>{props.name}</h1>
+    </header>
+  )
 }
 
 export default Header;
