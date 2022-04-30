26e1d9be4070a3ae8dde5355c3adb33e2837f4a2
Collins <mutaic84@gmail.com>
2022-04-30 19:38:57 +0300

    pass in minutes takesn to read as props to article

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,7 +3,7 @@ import Article from "./Article"
 
 function ArticleList(props){
   const article = props.posts.map((item) => {
-     return <Article key={item.id} title={item.title} date={item.date} preview={item.preview}/>
+     return <Article key={item.id} title={item.title} date={item.date} preview={item.preview} minutes={item.minutes}/>
       
   })
   return (
