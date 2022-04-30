9b8f09b2bfc9644f401b63e4de71f696835cce25
Collins <mutaic84@gmail.com>
2022-04-30 17:12:49 +0300

    display the about content on the page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header"
+import About from "./About"
 
 console.log(blogData);
 
 function App() {
   return (
-    <Header name={blogData.name}/>
+    <>
+      <Header name={blogData.name}/>
+      <About image={blogData.image} about={blogData.about} />
+    </>
   );
 }
 
